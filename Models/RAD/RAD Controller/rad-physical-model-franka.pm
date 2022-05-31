510002f66b9866935d0a2be043bfa70740182b88
Thomas Wright <tom.tdw@gmail.com>
2022-05-31 22:52:14 +0100

    Initial attempt at Franka model.

--- Models/RAD/RAD Controller/rad-physical-model-franka.pm
+++ Models/RAD/RAD Controller/rad-physical-model-franka.pm
@@ -1,106 +1,40 @@
 import physmod::joints::Revolute
 import physmod::actuators::*
+import physmod::math::*
 
-pmodel pmbaxter4 {
+
+pmodel pmfranka {
 	const PI: real = 3.1415	
-	const ARM_RADIUS: real = 0.069
-	const LENGTH_S: real = 0.27035
-	const LENGTH_E: real = 0.36435
-	const LENGTH_WA: real = 0.37429
-	const LENGTH_WB: real = 0.22953
-	const LENGTH_GRIPPER: real = 0.17
-	const GRIPPER_RADIUS: real = 0.022
-	const DISPLACEMENT1: real = 0.069
-	const DISPLACEMENT2: real = 0.069
-	const DISPLACEMENT3: real = 0.01
-	const Z_BASE: real = 1.0345
-	const X_BASE: real = 0.4
+	const ARM_RADIUS: real = 0.05
+	const GRIP_RADIUS: real = 0.02
+	const GRIP_LENGTH: real = 0.08
+	const Z_BASE: real = 0
+	const X_BASE: real = 0
+	const LEN1: real = 0.3330
+	const LEN2: real = 0.3160
+	const LEN3: real = 0.3840
+	const LEN4: real = 0.0825
+	const LEN5: real = 0.0880
+	const LEN6: real = 0.1070
+	const theta0: real = PI/2
+	const alpha0: real = PI/2
 	
-	local link LiBase {
-		def {}
-		local body CyB0 {
-			def {
-				cylinder ( radius = 0.2 , length = 0.9 )
-			}
-			pose {
-				x = 0
-				y = 0
-				z = 0.45
-				roll = 0.0
-				pitch = 0.0
-				yaw = 0.0
-			}
-		}
-		local body CyB1 {
-			def {
-				cylinder ( radius = ARM_RADIUS , length = 0.471 )
-			}
-			pose {
-				x = 0.2
-				y = 0
-				z = 0.95
-				roll = 0
-				pitch = -PI/2
-				yaw = 0.0
-			}
-		}
-		local body CyB2 {
-			def {
-				cylinder ( radius = ARM_RADIUS , length = 0.1 )
-			}
-			pose {
-				x = 0.4
-				y = 0
-				z = 0.9845
-				roll = 0.0
-				pitch = 0.0
-				yaw = 0.0
-			}
-		}
-		local body SpB {
-			def {
-				sphere ( radius = 0.2 )
-			}
-			pose {
-				x = 0
-				y = 0
-				z = 0.85
-				roll = 0.0
-				pitch = 0.0
-				yaw = 0.0
-			}
-		}
-		pose {
-			x = 0.0
-			y = 0.0
-			z = 0.0
-			roll = 0.0
-			pitch = 0.0
-			yaw = 0.0
-		}
-	}
-	local link LiS0 {
+	
+	local link Li0 {
 		def {
 		}
-		local body CyS {
+		local body Cy0 {
 			def {
-				cylinder ( radius = ARM_RADIUS , length = LENGTH_S )
+				cylinder ( radius = ARM_RADIUS , length = LEN1 )
 			}
 			pose {
 				x = 0.0
 				y = 0.0
-				z = LENGTH_S/2
+				z = LEN1/2
 				roll = 0.0
 				pitch = 0.0
 				yaw = 0.0
 			}
-		}
-		jref JS0 = Revolute {
-			instantiation AXIS = (|0,1,0|)
-			aref MS0 = SpeedControlMotor {
-				relation JS0.tau == MS0.T
-			}
-			flexibly connected to LiS1
 		}
 		pose {
 			x = X_BASE
@@ -111,159 +45,160 @@ pmodel pmbaxter4 {
 			yaw = 0.0
 		}
 	}
-	local link LiS1 {
-		def {}
-		jref JS1 = Revolute {
-			instantiation AXIS = (|0,0,1|)
-			aref MS1 = SpeedControlMotor {
-				relation JS1.tau == MS1.T
+	local link Li1 {
+		def {
+		}
+		local body Cy1 {
+			def {
+				cylinder ( radius = ARM_RADIUS , length = 2*ARM_RADIUS )
 			}
-			flexibly connected to LiE0
+			pose {
+				x = 0.0
+				y = 0.0
+				z = 0.0
+				roll = 0.0
+				pitch = 0.0
+				yaw = 0.0
+			}
 		}
 		pose {
-			x = X_BASE + DISPLACEMENT1
-			y = 0
-			z = Z_BASE + LENGTH_S
+			x = 0.0
+			y = 0.0
+			z = 0.333
+			roll = PI/2
+			pitch = 0.0
+			yaw = 0.0
+		}
+	}
+	local link Li2 {
+		def {
+		}
+		local body Cy2 {
+			def {
+				cylinder ( radius = ARM_RADIUS , length = LEN2 )
+			}
+			pose {
+				x = 0.0
+				y = 0.0
+				z = LEN2/2
+				roll = 0.0
+				pitch = 0.0
+				yaw = 0.0
+			}
+		}
+		pose {
+			x = 0.0
+			y = 0.0
+			z = 0.333
 			roll = 0.0
 			pitch = 0.0
 			yaw = 0.0
 		}
 	}
-	local link LiE0 {
-		def {}
-		jref JE0 = Revolute {
-			instantiation AXIS = (|0,0,1|)
-			aref ME0 = SpeedControlMotor {
-				relation JE0.tau == ME0.T
+	local link Li3 {
+		def {
+		}
+		local body Cy3 {
+			def {
+				cylinder ( radius = ARM_RADIUS , length = 2*ARM_RADIUS )
 			}
-			flexibly connected to LiE1
+			pose {
+				x = 0.0
+				y = 0.0
+				z = 0.0
+				roll = 0.0
+				pitch = 0.0
+				yaw = 0.0
+			}
 		}
 		pose {
-			x = X_BASE + DISPLACEMENT1
+			x = 0.0
 			y = 0.0
-			z = Z_BASE + LENGTH_S
+			z = 0.649
+			roll = PI/2
+			pitch = 0.0
+			yaw = 0.0
+		}
+	}
+	local link Li4 {
+		def {
+		}
+		local body Cy4 {
+			def {
+				cylinder ( radius = ARM_RADIUS , length = LEN3 )
+			}
+			pose {
+				x = 0.0
+				y = 0.0
+				z = LEN3/2
+				roll = 0.0
+				pitch = 0.0
+				yaw = 0.0
+			}
+		}
+		pose {
+			x = 0.0
+			y = 0.0
+			z = 0.649
 			roll = 0.0
 			pitch = 0.0
 			yaw = 0.0
 		}
-		local body CyE {
+	}
+	local link Li5 {
+		def {
+		}
+		local body Cy5 {
 			def {
-				cylinder ( radius = ARM_RADIUS , length = LENGTH_E )
+				cylinder ( radius = ARM_RADIUS , length = 2*ARM_RADIUS )
 			}
 			pose {
 				x = 0.0
 				y = 0.0
-				z = LENGTH_E/2
+				z = 0.0
 				roll = 0.0
 				pitch = 0.0
 				yaw = 0.0
 			}
 		}
-	}
-	local link LiE1 {
-		def {}
-		jref JE1 = Revolute {
-			instantiation AXIS = (|0,1,0|)
-			aref ME1 = SpeedControlMotor {
-				relation JE1.tau == ME1.T
-			}
-			flexibly connected to LiW0
-		}
 		pose {
-			x = X_BASE + DISPLACEMENT1 + DISPLACEMENT2
+			x = 0.0
 			y = 0.0
-			z = Z_BASE + LENGTH_S + LENGTH_E
-			roll = 0.0
-			pitch = PI/2
+			z = LEN1 + LEN2 + LEN3 
+			roll = PI/2
+			pitch = 0.0
 			yaw = 0.0
 		}
 	}
-	local link LiW0 {
-		def {}
-		jref JW0 = Revolute {
-			instantiation AXIS = (|0,0,1|)
-			aref MW0 = SpeedControlMotor {
-				relation JW0.tau == MW0.T
-			}
-			flexibly connected to LiW1
+	local link Li6 {
+		def {
 		}
-		pose {
-			x = X_BASE + DISPLACEMENT1 + DISPLACEMENT2
-			y = 0.0
-			z = Z_BASE + LENGTH_S + LENGTH_E
-			roll = 0.0
-			pitch = PI/2
-			yaw = 0.0
-		}
-		local body CyWA {
+		local body Cy6 {
 			def {
-				cylinder ( radius = ARM_RADIUS , length = LENGTH_WA )
+				cylinder ( radius = ARM_RADIUS , length = LEN4 )
 			}
 			pose {
 				x = 0.0
 				y = 0.0
-				z = LENGTH_WA/2
+				z = LEN4/2
 				roll = 0.0
 				pitch = 0.0
 				yaw = 0.0
 			}
 		}
-	}
-	local link LiW1 {
-		def {}
-		jref JW1 = Revolute {
-			instantiation AXIS = (|0,1,0|)
-			aref MW1 = SpeedControlMotor {
-				relation JW1.tau == MW1.T
-			}
-			flexibly connected to LiW2
-		}
 		pose {
-			x = X_BASE + DISPLACEMENT1 + DISPLACEMENT2 + LENGTH_WA
+			x = 0.0
 			y = 0.0
-			z = Z_BASE + LENGTH_S + LENGTH_E + DISPLACEMENT3
-			roll = 0.0
-			pitch = PI/2
-			yaw = 0.0
-		}
-		local body CyWB {
-			def {
-				cylinder ( radius = ARM_RADIUS , length = LENGTH_WB )
-			}
-			pose {
-				x = 0.0
-				y = 0.0
-				z = LENGTH_WB/2
-				roll = 0.0
-				pitch = 0.0
-				yaw = 0.0
-			}
+			z = LEN1 + LEN2 + LEN3
+			roll = PI/2
+			pitch = 0.0
+			yaw = PI/2
 		}
 	}
-	local link LiW2 {
-		def {}
-		pose {
-			x = X_BASE + DISPLACEMENT1 + DISPLACEMENT2 + LENGTH_WA + LENGTH_WB
-			y = 0.0
-			z = Z_BASE + LENGTH_S + LENGTH_E + DISPLACEMENT3
-			roll = 0.0
-			pitch = PI/2
-			yaw = 0.0
+	local link Li7 {
+		def {
 		}
-		local body CyEE {
-			def {
-				cylinder ( radius = GRIPPER_RADIUS , length = LENGTH_GRIPPER )
-			}
-			pose {
-				x = 0.0
-				y = 0.0
-				z = LENGTH_GRIPPER/2
-				roll = 0.0
-				pitch = 0.0
-				yaw = 0.0
-			}
-		}
-		local body SpEE {
+		local body Sp7 {
 			def {
 				sphere ( radius = ARM_RADIUS )
 			}
@@ -275,6 +210,52 @@ pmodel pmbaxter4 {
 				pitch = 0.0
 				yaw = 0.0
 			}
+		}
+		local body Cy7 {
+			def {
+				cylinder ( radius = ARM_RADIUS , length = LEN6 )
+			}
+			pose {
+				x = 0.0
+				y = 0.0
+				z = LEN6/2
+				roll = 0.0
+				pitch = 0.0
+				yaw = 0.0
+			}
+		}
+		pose {
+			x = LEN4
+			y = 0.0
+			z = LEN1 + LEN2 + LEN3
+			roll = 0.0
+			pitch = -PI
+			yaw = 0.0
+		}
+	}
+	local link LiF {
+		def {
+		}
+		local body CyF {
+			def {
+				cylinder ( radius = GRIP_RADIUS , length = GRIP_LENGTH )
+			}
+			pose {
+				x = 0.0
+				y = 0.0
+				z = GRIP_LENGTH/2
+				roll = 0.0
+				pitch = 0.0
+				yaw = 0.0
+			}
+		}
+		pose {
+			x = LEN4
+			y = 0.0
+			z = LEN1 + LEN2 + LEN3 - LEN6
+			roll = 0.0
+			pitch = -PI
+			yaw = 0.0
 		}
 	}
 }
